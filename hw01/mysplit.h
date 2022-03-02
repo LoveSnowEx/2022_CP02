ebaba5a46a711ce978a72dfc882e93d70a32b5c4
LoveSnowEx <lsktw22@gamil.com>
2022-03-03 00:43:47 +0800

    (fix): comment debug print

--- hw01/mysplit.h
+++ hw01/mysplit.h
@@ -14,7 +14,7 @@ pSeparator) {
 		for(i32 i = 0; i < len;) {
 			i += strspn(cpy+i, " \t\n");
 			if(!cpy[i]) break;
-			printf("%d: %s\n", cnt, cpy+i);
+			// printf("%d: %s\n", cnt, cpy+i);
 			ppList[cnt++] = cpy+i;
 			i += strcspn(cpy+i, " \t\n");
 			cpy[i++] = '\0';
@@ -27,7 +27,7 @@ pSeparator) {
 		}
 		seplen = strlen(pSeparator);
 		for(i32 i = 0; i <= len;) {
-			printf("%d: %s\n", cnt, cpy+i);
+			// printf("%d: %s\n", cnt, cpy+i);
 			ppList[cnt++] = cpy+i;
 			char *nxt = strstr(cpy+i, pSeparator);
 			if(!nxt) break;
