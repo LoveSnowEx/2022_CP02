ed1e0fb55ec83b2a63143acc9f2782348525c7b6
LoveSnowEx <lsktw22@gamil.com>
2022-03-02 23:01:13 +0800

    (feat): finish hw0102.c

--- hw01/hw0102.c
+++ hw01/hw0102.c
@@ -2,6 +2,20 @@
 #include "color.h"
 #define MAXSIZE 8192
 
+#define max(a, b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a > _b ? _a : _b;       \
+})
+
+#define min(a, b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a < _b ? _a : _b;       \
+})
+
 int main() {
 	char s[MAXSIZE], s1[MAXSIZE] = "", s2[MAXSIZE] = "", keyword[MAXSIZE], newword[MAXSIZE];
 	printf("Please enter the string:\n");
@@ -10,21 +24,29 @@ int main() {
 	while (!fgets_n(keyword, MAXSIZE, stdin));
 	printf("Please enter the new word:\n");
 	while (!fgets_n(newword, MAXSIZE, stdin));
-	i32 len = strlen(s), kwlen = strlen(keyword);
-	// print("Original:\n");
-	char cpy = strdup(s);
-	for(char *cur = cpy; cur-s < len;) {
-		char *nxt = strcasestr(cur, keyword);
-		if(!nxt || (nxt+kwlen <= len && (nxt[kwlen] != ',' || nxt[kwlen] != ' '))) {
-			strncat(s1, cur, nxt+kwlen-cur);
-			strncat(s2, cur, nxt+kwlen-cur);
-			cur = nxt+kwlen;
+	i32 len = strlen(s), kwlen = strlen(keyword), nwlen = strlen(newword);
+	for(char *cur = s; cur-s < len;) {
+		i32 cnt = strspn(cur, " ,.");
+		strncat(s1, cur, cnt);
+		strncat(s2, cur, cnt);
+		cur += cnt;
+
+		cnt = strcspn(cur, " ,.");
+		if(strncasecmp(cur, keyword, max(kwlen, cnt)) == 0) {
+			strcat(s1, CLR_GRN);
+			strcat(s2, CLR_RED);
+			strncat(s1, cur, cnt);
+			strncat(s2, newword, nwlen);
+			strcat(s1, CLR_RST);
+			strcat(s2, CLR_RST);
 		}
 		else {
-			strncatf(s1, CLR_RED"%.*s", cur, nxt+kwlen-cur);
-			strncatf(s2, CLR_GRN"%.*s", cur, nxt+kwlen-cur);
+			strncat(s1, cur, cnt);
+			strncat(s2, cur, cnt);
 		}
+		cur += cnt;
 	}
-	printf("%s\n", res);
+	printf("Original:\n%s\n", s1);
+	printf("New:\n%s\n", s2);
 	return 0;
 }
